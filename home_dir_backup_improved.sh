e5823da8a0e1c679cdc9d0cb4756a3ef6c39bef2
Tracy Vierra <tracyvierra@yahoo.com>
2022-01-31 17:47:07 -0500

    more fixes

--- home_dir_backup_improved.sh
+++ home_dir_backup_improved.sh
@@ -11,13 +11,13 @@
 # home_dir_backup.sh
 currentdir=$(pwd)
 
-echo "Hello, $USER"
+echo "Hello, ${USER^}!"
 echo "I will now back up your home directory, $HOME"
 echo  ""
 echo "You are running this script from ${currentdir}"
 echo "Therefore, I will save the file in the current directory: ${currentdir}"
 
-tar -cf ${currentdir}"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+tar -cf ${currentdir}/"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
 
 echo "Backup Completed Successfully."
 
