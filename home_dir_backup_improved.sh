07ca54aa1b966ddd01bca8f1223a6785322ef0bb
Tracy Vierra <tracyvierra@yahoo.com>
2022-01-31 17:35:16 -0500

    was missing a space in the filename for tar

--- home_dir_backup_improved.sh
+++ home_dir_backup_improved.sh
@@ -17,7 +17,7 @@ echo  ""
 echo "You are running this script from ${currentdir}"
 echo "Therefore, I will save the file in the current directory: ${currentdir}"
 
-tar -cf ${currentdir}"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+tar -cf ${currentdir} "$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
 
 echo "Backup Completed Successfully."
 
