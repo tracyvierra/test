34d4d932bd7c22303f1fef1c9055de8e432346d2
Tracy Vierra <tracyvierra@yahoo.com>
2022-01-31 17:25:49 -0500

    added completion message to improved backup script

--- home_dir_backup_improved.sh
+++ home_dir_backup_improved.sh
@@ -19,4 +19,6 @@ echo "Therefore, I will save the file in the current directory: ${currentdir}"
 
 tar -cf ${currentdir}"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
 
+echo "Backup Completed Successfully."
+
 exit 0
