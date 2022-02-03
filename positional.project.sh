ff629dae2cdbcf165e8e50ec427de7cbf1eb2aa8
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-03 15:08:28 -0800

    added positional.project.sh

---
+++ positional.project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Author: Tracy Vierra
+# Date Created: 2/3/2022
+# Date Modified: 2/3/2022
+
+# Description:
+# This script will calculate the positional parameters of a command line using the first positional parameter as the operand
+
+# Usage:
+# positional.project.sh [operand] [number1] [number2] [number3] [number4] [number5] [number6] [number7] [number8] [number9]
+
+echo ($(( $2 $1 $3 $1 $4 $1 $5 $1 $6 $1 $7 $1 $8 $1 $9 )))
