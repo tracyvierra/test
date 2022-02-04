97d350019cb716a1483f50fe956c991fd1853364
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-04 09:57:02 -0800

    updated positional.project.sh with solution

--- positional.project.sh
+++ positional.project.sh
@@ -10,7 +10,9 @@
 # Usage:
 # positional.project.sh [operand] [number1] [number2] [number3] [number4] [number5] [number6] [number7] [number8] [number9]
 
-echo "$(($2 $1 $3 $1 $4 $1 $5 $1 $6 $1 $7 $1 $8 $1 $9))"
+# My solution:
+# echo "$(($2 $1 $3 $1 $4 $1 $5 $1 $6 $1 $7 $1 $8 $1 $9))"
 
-
+# Solution with method to allow blank positional parameters:
+echo $(( ${2:-0} $1 ${3:-0} $1 ${4:-0} $1 ${5:-0} $1 ${6:-0} $1 ${7:-0} $1 ${8:-0} $1 ${9:-0} ))
 
