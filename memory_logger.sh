c9b8c2707ce4acdfce4293acf69ea390a3edeb60
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-08 13:16:05 -0800

    adding memory_logger.sh

---
+++ memory_logger.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: Tracy Vierra
+# Date Created: 2/8/2022
+# Date Modified: 2/8/2022
+
+# Description:
+
+# Usage:
+# ./memory_logger.sh
+
+if [ -d $HOME/performance ]; then
+	echo "$HOME/performance directory exists"
+else 
+	echo "$HOME/performance directory does not exist"
+	mkdir $HOME/performance
+fi
+
+free >> $HOME/performance/memory.log
+
+cat $HOME/performance/memory.log 
+ 
+
+
+
+
+
