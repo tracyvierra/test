d2ee30b2a59492407b73a2c0460752975e48df7a
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-07 16:13:49 -0800

    added usage and description for get_extensions.sh

--- get_extensions.sh
+++ get_extensions.sh
@@ -5,8 +5,11 @@
 # Date Modified: 2/7/2022
 
 # Description:
+# This script will get the extensions of all the users that run it and
+# save the data to a file named extensions.csv in their home directory.
 
 # Usage:
+# ./get_extensions.sh
 
 read -t 15 -p "Please enter your first name : " firstname
 read -t 15 -p "Please enter your last name : " lastname
