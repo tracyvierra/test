81d6b1c4494a045770f7e3aefc64c03f6dfcc3ed
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-07 16:04:37 -0800

    finished read.sh

--- get_extensions.sh
+++ get_extensions.sh
@@ -8,6 +8,14 @@
 
 # Usage:
 
+read -t 15 -p "Please enter your first name : " firstname
+read -t 15 -p "Please enter your last name : " lastname
+read -n 4 -t 15 -p "Please enter your extension number (4 digits) : " extension
+echo " "
+read -s -n 4 -t 15 -p "Please enter your access code (4 digits) : " accesscode
+echo " "
+
+echo "$firstname, $lastname, $extension, $accesscode" >> $HOME/extensions.csv
 
 
 
