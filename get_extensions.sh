e2f04181d32f9c22923a4e093d3d7936508bdbfa
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-07 16:43:18 -0800

    done with get_extensions.sh

--- get_extensions.sh
+++ get_extensions.sh
@@ -18,11 +18,13 @@ select type in Headset Handheld; do
 	echo "You selected $type"
 	break
 done
+echo
 PS3="Please enter your department : "
 select dept in "Finance" "Sales" "Customer Service" "Marketing"; do
 	echo "You selected $dept"
 	break
 done
+echo
 PS3=" "
 read -N 4 -t 15 -p "Please enter your extension number (4 digits) : " extension
 echo " "
