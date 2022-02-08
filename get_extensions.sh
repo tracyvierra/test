a4a68fab4ae1f0de0e513af177e1934a33ea9ee6
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-07 16:37:47 -0800

    updated get_extensions.sh for the select project

--- get_extensions.sh
+++ get_extensions.sh
@@ -13,12 +13,23 @@
 
 read -t 15 -p "Please enter your first name : " firstname
 read -t 15 -p "Please enter your last name : " lastname
+PS3="Please enter what type of phone you would like : "
+select type in Headset Handheld; do
+	echo "You selected $type"
+	break
+done
+PS3="Please enter your department : "
+select dept in "Finance" "Sales" "Customer Service" "Marketing"; do
+	echo "You selected $dept"
+	break
+done
+PS3=" "
 read -N 4 -t 15 -p "Please enter your extension number (4 digits) : " extension
 echo " "
 read -s -N 4 -t 15 -p "Please enter your access code (4 digits) : " accesscode
 echo " "
 
-echo "$firstname,$lastname,$extension,$accesscode" >> $HOME/extensions.csv
+echo "$firstname,$lastname,$type,$dept,$extension,$accesscode" >> $HOME/extensions.csv
 
 
 
