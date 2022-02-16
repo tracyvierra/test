def67e85842b71f7bb2ae9749714b76800467500
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-16 11:59:23 -0800

    added update_system_2.sh using a function.

---
+++ scripts/update_system_2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Author: Tracy Vierra
+# Date Created: 2/16/2022
+# Date Modified: 2/16/2022
+
+# Description:
+# This script will run apt-get update and dist-upgrade and reboot if a reboot is required.
+
+# Usage: 
+# ./update_system.sh 
+
+update_system () {
+        apt-get update -y
+        apt-get dist-upgrade -y
+        if [ -f /var/run/reboot-required ]; then
+                shutdown --reboot
+        fi
+}
+
+#apt-get update -y
+#apt-get dist-upgrade -y
+
+#if [ -f /var/run/reboot-required ]; then
+#         shutdown --reboot
+#fi
+
+
+
+
+
+
+
+
