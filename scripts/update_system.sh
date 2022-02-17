9a0f2f0661113ab0a73194fd311f1e595cd0cd45
Tracy Vierra <tracyvierra@yahoo.com>
2022-02-17 11:04:04 -0800

    fixed update scripts

--- scripts/update_system.sh
+++ scripts/update_system.sh
@@ -5,17 +5,31 @@
 # Date Modified: 2/16/2022
 
 # Description:
-# This script will run apt-get update and dist-upgrade and reboot if a reboot is required.
+# This script will run apt-get update and dist-upgrade, also autoremove and autoclean and reboot if a reboot is required.
 
 # Usage: 
 # ./update_system.sh 
 
-apt-get update -y
-apt-get dist-upgrade -y
-
-if [ -f /var/run/reboot-required ]; then
-         shutdown --reboot
-fi
+update_system () {
+        apt-get update -y
+        apt-get dist-upgrade -y
+        apt-get autoremove -y
+        apt-get autoclean -y
+        if [ -f /var/run/reboot-required ]; then
+                shutdown --reboot
+        fi
+}
+#
+#
+{ 
+        echo "Updating System on $(date)"
+        echo " "
+        update_system 
+        echo " "
+        echo "Update System Complete on $(date)"
+        echo "_____________________________________________"
+        echo " "
+}  >> "$HOME"/update_system_"$(hostname)".log 2>&1
 
 
 
